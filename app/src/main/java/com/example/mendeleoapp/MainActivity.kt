80c29ba2b7ebfe5d84baf819129bb54283eca9f6
Claire-101 <muinamiclaire@gmail.com>
2025-03-04 20:11:18 +0300

    initial commit

---
+++ app/src/main/java/com/example/mendeleoapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.mendeleoapp
+
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Find button and set click listener
+        val button = findViewById<Button>(R.id.goToHome)
+        button.setOnClickListener {
+            val intent = Intent(this, details::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mendeleoapp/home.kt
@@ -0,0 +1,24 @@
+package com.example.mendeleoapp
+
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
